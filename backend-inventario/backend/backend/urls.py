b251d896639c2b30d603681a812d8a2d029069e4
Juan Camilo Garcia <juankg19@gmail.com>
2025-07-16 21:04:13 -0500

    Reparo ruta inventario

--- backend-inventario/backend/backend/urls.py
+++ backend-inventario/backend/backend/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/inventrio/', include('inventario.urls')),
+    path('api/inventario/', include('inventario.urls')),
 
 ]
 
